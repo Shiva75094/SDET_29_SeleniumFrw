3998a57b8fb9b53b7c9a2181c767a2ce4a791c5d
Admin <Admin@DESKTOP-KEV4HHF>
2022-01-27 17:54:06 +0530

    Uesr2 First change

--- seleniumCRM_Framework/src/test/java/com/crm/orgTest/OrganizationTest.java
+++ seleniumCRM_Framework/src/test/java/com/crm/orgTest/OrganizationTest.java
@@ -17,5 +17,11 @@ public class OrganizationTest {
 	public void modifyOrgTest() {
 		System.out.println("execute modifyOrgTest");
 	}
+	
+	@Test(groups = "regressionTest")
+	public void deleteOrgTest() {
+		System.out.println("execute deleteOrgTest");
+	}
+
 
 }
